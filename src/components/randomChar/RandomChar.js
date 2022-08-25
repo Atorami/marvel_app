b964c3adf5437ab1a9dd227104a67ba1dccc8ad6
Atorami <atoramiseo@mail.ru>
2022-08-25 21:55:47 +0200

    added CharList loading, added onClick for button

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -8,10 +8,6 @@ import mjolnir from '../../resources/img/mjolnir.png';
 
 
 class RandomChar extends Component{
-    constructor(props){
-        super(props)
-        this.updateChar();
-    }
 
     state = {
         char: {},
@@ -19,6 +15,10 @@ class RandomChar extends Component{
         error: false
     }
     marvelService = new MarvelService();
+
+    componentDidMount(){
+        this.updateChar();
+    }
 
     onCharLoaded = (char) =>{
         this.setState({char, loading: false})
@@ -35,6 +35,7 @@ class RandomChar extends Component{
     onError = () =>{
         this.setState({loading: false, error: true})
     }
+    
 
     render(){
         const {char, loading, error} = this.state
@@ -55,7 +56,7 @@ class RandomChar extends Component{
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button onClick={this.updateChar} className="button button__main">
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
@@ -67,10 +68,14 @@ class RandomChar extends Component{
 
 const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki} = char
+    let imgStyle = {'objectFit' : 'cover'};
+        if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'contain'};
+    }
 
     return(
         <div className="randomchar__block">
-                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+                    <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
                     <div className="randomchar__info">
                         <p className="randomchar__name">{name}</p>
                         <p className="randomchar__descr">
